1bb0d5b2c2636882ff00905ecfa0645f9238da12
AyazKemal <kemayaz@gmail.com>
2024-06-25 20:00:45 +0300

    Before ng-Gallery

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,39 +1,21 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css',
-    imports: [RegisterComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  imports: [RegisterComponent]
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient)
-registerMode=false;
-users:any;
+export class HomeComponent {
+  registerMode = false;
 
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
 
-ngOnInit(): void {
- this.getUsers();
- }
-
-
-registerToggle(){
-  this.registerMode = !this.registerMode;
-}
-
-cancelRegisterMode(event: boolean){
-  this.registerMode = event;
-}
-
-getUsers(){
-  this.http.get('https://localhost:5001/api/Users').subscribe({
-    next: response => this.users = response,
-    error: error => console.log(error),
-    complete: () => console.log('Request has completed')
-  })
-}
-
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
 }
